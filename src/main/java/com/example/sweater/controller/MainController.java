6046ba588b70e2eac80549083ee18fa45e4ca9e1
DmitryZzz <yes.zimin@gmail.com>
2020-11-28 21:35:34 +0300

    Spring Boot: оформляем UI с Bootstrap

--- src/main/java/com/example/sweater/controller/MainController.java
+++ src/main/java/com/example/sweater/controller/MainController.java
@@ -92,6 +92,6 @@ public class MainController {
         Iterable<Message> messages = messageRepo.findAll();
         model.put("messages", messages);
 
-        return "main";
+        return "redirect:/main";
     }
 }
