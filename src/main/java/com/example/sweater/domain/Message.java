20e642d6a6e4ec434e3a07bba6605fe699b6a509
DmitryZzz <yes.zimin@gmail.com>
2020-12-12 00:30:27 +0300

    Spring Boot: bean validation, шифрование паролей

--- src/main/java/com/example/sweater/domain/Message.java
+++ src/main/java/com/example/sweater/domain/Message.java
@@ -1,13 +1,22 @@
 package com.example.sweater.domain;
 
+import org.hibernate.validator.constraints.Length;
+
 import javax.persistence.*;
+import javax.validation.constraints.NotBlank;
 
 @Entity
 public class Message {
     @Id
     @GeneratedValue(strategy = GenerationType.AUTO)
     private Long id;
+
+    @NotBlank(message = "Please fill the message")
+    @Length(max = 2048, message = "Message is too long (more than 2kB)")
     private String text;
+
+    @NotBlank(message = "Please fill the tag")
+    @Length(max = 255, message = "tag is too long (more than 255)")
     private String tag;
 
     @ManyToOne(fetch = FetchType.EAGER)
